0633933e976e360673fb480af715c0ffda3d360f
Danil Gulin <d.nk.gulin@gmail.com>
2024-07-13 19:30:47 +0200

    Add prefix to local function names

--- integration/ttyhmaster_punbb_bridge/functions.php
+++ integration/ttyhmaster_punbb_bridge/functions.php
@@ -2,7 +2,7 @@
 const TTYH_MASTER_HOST = 'http://example.com';
 const TTYH_MASTER_API_TOKEN = 'example-token';
 
-function ttyh_master_get_response_code($http_response_header): int
+function _ttyh_master_get_response_code($http_response_header): int
 {
     if (is_array($http_response_header)) {
         $parts = explode(' ', $http_response_header[0]);
@@ -14,7 +14,7 @@ function ttyh_master_get_response_code($http_response_header): int
 }
 
 // Returns [ 'code' => 200, 'payload' => [ ... ] ] or [ 'code' => int ]
-function ttyh_master_get($endpoint): array
+function _ttyh_master_get($endpoint): array
 {
     $url = TTYH_MASTER_HOST . '/' . $endpoint;
     $auth_header = 'Authorization: Bearer ' . TTYH_MASTER_API_TOKEN;
@@ -27,7 +27,7 @@ function ttyh_master_get($endpoint): array
     ];
 
     $response = file_get_contents($url, false, stream_context_create($options));
-    $code = ttyh_master_get_response_code($http_response_header);
+    $code = _ttyh_master_get_response_code($http_response_header);
 
     if ($code != 200) {
         return ['code' => $code];
@@ -36,7 +36,7 @@ function ttyh_master_get($endpoint): array
     return ['code' => $code, 'payload' => json_decode($response)];
 }
 
-function ttyh_master_post($endpoint, $payload): int
+function _ttyh_master_post($endpoint, $payload): int
 {
     $url = TTYH_MASTER_HOST . '/' . $endpoint;
     $auth_header = 'Authorization: Bearer ' . TTYH_MASTER_API_TOKEN;
@@ -50,7 +50,7 @@ function ttyh_master_post($endpoint, $payload): int
     ];
 
     file_get_contents($url, false, stream_context_create($options));
-    return ttyh_master_get_response_code($http_response_header);
+    return _ttyh_master_get_response_code($http_response_header);
 }
 
 function ttyh_master_create_player($name, $pwd_hash, $pwd_salt): int
@@ -62,13 +62,13 @@ function ttyh_master_create_player($name, $pwd_hash, $pwd_salt): int
             'salt' => $pwd_salt,
         ]
     ];
-    return ttyh_master_post('ttyh/player/create', $payload);
+    return _ttyh_master_post('ttyh/player/create', $payload);
 }
 
-function ttyh_master_update_player($name, $payload): int
+function _ttyh_master_update_player($name, $payload): int
 {
     $endpoint = 'ttyh/player/' . urlencode($name) . '/update';
-    return ttyh_master_post($endpoint, $payload);
+    return _ttyh_master_post($endpoint, $payload);
 }
 
 function ttyh_master_update_player_name($name, $new_name): int
@@ -76,7 +76,7 @@ function ttyh_master_update_player_name($name, $new_name): int
     $payload = [
         'player_name' => $new_name
     ];
-    return ttyh_master_update_player($name, $payload);
+    return _ttyh_master_update_player($name, $payload);
 }
 
 function ttyh_master_update_player_password($name, $pwd_hash, $pwd_salt): int
@@ -87,7 +87,7 @@ function ttyh_master_update_player_password($name, $pwd_hash, $pwd_salt): int
             'salt' => $pwd_salt,
         ]
     ];
-    return ttyh_master_update_player($name, $payload);
+    return _ttyh_master_update_player($name, $payload);
 }
 
 function ttyh_master_update_player_is_mojang($name, $is_mojang): int
@@ -96,11 +96,11 @@ function ttyh_master_update_player_is_mojang($name, $is_mojang): int
         'is_mojang' => $is_mojang
     ];
 
-    return ttyh_master_update_player($name, $payload);
+    return _ttyh_master_update_player($name, $payload);
 }
 
 // returns [ 'code' => 200, 'payload' => [ 'player_id' => string, 'is_mojang' => bool ] ] or [ 'code' => int ]
 function ttyh_master_query_player($name): array
 {
-    return ttyh_master_get('ttyh/player/' . urlencode($name));
+    return _ttyh_master_get('ttyh/player/' . urlencode($name));
 }
