a2e7655d1dbfdb7ed3564f82771e26025de27974
Danil Gulin <d.nk.gulin@gmail.com>
2024-07-13 22:14:39 +0200

    Add hook for user renaming

---
+++ integration/ttyhmaster_punbb_bridge/pf_change_details_qr_update_user.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+
+if ($username_updated) {
+    $_response_code = ttyh_master_update_player_name($old_username, $form['username']);
+    if ($_response_code != 200) {
+        error("Failed to update user name on master server ({$_response_code})", __FILE__, __LINE__);
+    }
+}
