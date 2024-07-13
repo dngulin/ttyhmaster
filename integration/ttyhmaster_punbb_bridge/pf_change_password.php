c959c4a40c7c01c6c45ac81b2d1abb8e27c54d9a
Danil Gulin <d.nk.gulin@gmail.com>
2024-07-13 22:13:51 +0200

    Show error if user password is not changed on master server

--- integration/ttyhmaster_punbb_bridge/pf_change_password.php
+++ integration/ttyhmaster_punbb_bridge/pf_change_password.php
@@ -1,3 +1,7 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
-ttyh_master_update_player_password($user['username'], $new_password_hash, $user['salt']);
+
+$_response_code = ttyh_master_update_player_password($user['username'], $new_password_hash, $user['salt']);
+if ($_response_code != 200) {
+    error("Failed to update user password on master server ({$_response_code})", __FILE__, __LINE__);
+}
