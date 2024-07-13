81276b48b5b24c7c7f90e5f48b608f87403fa804
Danil Gulin <d.nk.gulin@gmail.com>
2024-07-13 20:26:12 +0200

    Fix registration hook

--- integration/ttyhmaster_punbb_bridge/rg_register_pre_login_redirect.php
+++ integration/ttyhmaster_punbb_bridge/rg_register_pre_login_redirect.php
@@ -1,15 +1,7 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 
-$query = [
-    'SELECT' => 'password',
-    'FROM' => 'users',
-    'WHERE' => "username = '{$forum_db->escape($username)}'",
-];
-$result = $forum_db->query_build($query) or exit(21);
-$password = $forum_db->fetch_assoc($result)['password'];
-
-$code = ttyh_master_create_player($username, $password, $salt);
+$code = ttyh_master_create_player($username, $password_hash, $salt);
 if ($code != 200) {
-    error_log("Failed to create player `{$username}`: response code {$code}");
+    error_log("Failed to link player `{$username}`: response code {$code}");
 }
