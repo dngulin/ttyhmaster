a4d7dc2383e7713950e095942d72a135e9d557bc
Danil Gulin <d.nk.gulin@gmail.com>
2024-08-05 20:54:02 +0200

    Allows hooks to fail

--- integration/ttyhmaster_punbb_bridge/pf_change_password.php
+++ integration/ttyhmaster_punbb_bridge/pf_change_password.php
@@ -3,5 +3,5 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 
 $_response_code = ttyh_master_update_player_password($user['username'], $new_password_hash, $user['salt']);
 if ($_response_code != 200) {
-    error("Failed to update user password on master server ({$_response_code})", __FILE__, __LINE__);
+    error_log("Failed to update user password on master server ({$_response_code})");
 }
--- integration/ttyhmaster_punbb_bridge/rg_register_pre_add_user.php
+++ integration/ttyhmaster_punbb_bridge/rg_register_pre_add_user.php
@@ -3,5 +3,5 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 
 $_response_code = ttyh_master_create_player($username, $password_hash, $salt);
 if ($_response_code != 200) {
-    error("Failed to create a new user `{$username}` on master server ({$_response_code})", __FILE__, __LINE__);
+    error_log("Failed to create a new user `{$username}` on master server ({$_response_code})");
 }
